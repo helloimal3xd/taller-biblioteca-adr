520e9df768cdb14cd63f96cb96906e693f8a129c
helloimal3xd <alejorey2002@hotmail.com>
2026-09-24 20:47:50 -0500

    se romre R1 a proposito: prestamos importa avisos

--- src/prestamos.js
+++ src/prestamos.js
@@ -2,6 +2,7 @@
 // IMPORTANTE: este módulo NO importa avisos.js a propósito (ver ADR-001 del taller).
 
 import { hayDisponible, reservarCopia, liberarCopia, buscarLibro } from './catalogo.js';
+import { avisarPrestamo } from './avisos.js';
 
 const prestamos = [];
 let siguienteId = 1;
